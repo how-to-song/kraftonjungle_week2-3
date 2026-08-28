7e038b13acf1a0612859bee4a8456781d2bb66b8
송하우 <song03621@gmail.com>
2026-08-29 00:59:10 +0900

    배열의 최댓값 찾기 closes #16

--- week2/1. basic/11_divide_conquer.py
+++ week2/1. basic/11_divide_conquer.py
@@ -36,19 +36,24 @@ def find_max_divide_conquer(arr, left, right):
         최댓값
     """
     # TODO: base case - 원소가 하나면 그 값 반환
-    pass
-    
+    if len(arr) == 1:
+        return arr[0]
     # TODO: 중간 지점 계산
-    pass
+    mid = (right - left) // 2
     
     # TODO: 왼쪽 절반의 최댓값
-    pass
-        
+    max_left = 0
+    for i in range(left, mid + 1):
+        if (max_left < arr[i]):
+            max_left = arr[i]
     # TODO: 오른쪽 절반의 최댓값
-    pass
+    max_right = 0
+    for i in range(mid, right+1):
+        if max_right < arr[i]:
+            max_right = arr[i]
     
     # TODO: 둘 중 큰 값 반환
-    pass
+    return max_right if max_right > max_left else max_left
 
 # 테스트 케이스
 if __name__ == "__main__":
