9fc1f36631e1dbe85a6f8d6846908c720f346b3b
송하우 <song03621@gmail.com>
2026-08-28 22:35:28 +0900

    최대공약수(GCD)와 최소공배수(LCM) closes #14

--- week2/1. basic/09_number_theory.py
+++ week2/1. basic/09_number_theory.py
@@ -37,7 +37,8 @@ def gcd(a, b):
     # TODO: 유클리드 호제법 구현
     # base case: b가 0이면 a 반환
     # recursive를 이용 
-    pass
+    if (b == 0): return a
+    return gcd(b, a % b) 
 
 def gcd_iterative(a, b):
     """
@@ -51,7 +52,12 @@ def gcd_iterative(a, b):
     """
     # TODO: 반복문으로 구현
     # b가 0이 될 때까지 반복
-    pass
+    while b != 0:
+        temp = a
+        a = b
+        b = temp % b
+        
+    return a
 
 def lcm(a, b):
     """
@@ -64,7 +70,7 @@ def lcm(a, b):
         최소공배수
     """
     # TODO: LCM 계산
-    pass
+    return int((a * b) / gcd(a, b))
 
 def extended_gcd(a, b):
     """
@@ -81,7 +87,22 @@ def extended_gcd(a, b):
     # base case: b가 0이면 (a, 1, 0) 반환    
     # recursive case
     # 역추적하며 x, y 계산
-    pass
+    
+    
+    if b == 0: return a, 1, 0
+    
+    g = gcd(a, b)
+    x = 1
+    y = 0
+    
+    while a * x + b * y != g:
+        temp = x
+        x = y
+        y = temp - a//b * y
+    
+    return g, x, y
+    
+    
 
 def is_prime(n):
     """
@@ -97,7 +118,19 @@ def is_prime(n):
     # n이 2보다 작으면 False
     # 2부터 sqrt(n)까지 나누어 떨어지는지 확인    
     # 3부터 sqrt(n)까지 홀수만 확인
-    pass 
+    
+    if n < 2: return False
+    if n == 2: return True
+    if n % 2 == 0: return False
+    
+    sqrt_n = int(n ** 0.5) + 1
+        
+    for i in range(3, sqrt_n, 2):
+        if n % i == 0: return False
+        
+    return True
+    
+
 
 # 테스트 케이스
 if __name__ == "__main__":
